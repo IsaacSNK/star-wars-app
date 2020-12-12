e1b07e818e7b03dcde9860d0ca8f4be23419bd22
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2020-12-11 20:27:45 -0600

    Add npm install call

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "1/2 Starting the build of the react application..."
+
+echo "1/3 Getting dependencies..."
+npm install
+
+echo "2/3 Starting the build of the react application..."
 npm run build
 
-echo "2/2 Packing the application into a docker image..."
+echo "2/3 Packing the application into a docker image..."
 docker build . -t star-wars-server
 
-echo "2/2 Steps completed. Bye"
+echo "3/3 Steps completed. Bye"
